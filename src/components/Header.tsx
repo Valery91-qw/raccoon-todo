db8a82bdb4450279c57b263b26ade18a3cf5b313
Valery91-qw <vakriv91@gmail.com>
2023-01-18 20:42:58 +0300

    feat: create header, add base styles.

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import {
+  AppBar, ButtonBase, TextField, Toolbar,
+} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+export default function Header() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <TextField variant="standard" label="ToDo name" required />
+        <ButtonBase>
+          <AddIcon fontSize="large" />
+        </ButtonBase>
+      </Toolbar>
+    </AppBar>
+  );
+}
